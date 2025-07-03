a6aafdae9e979fbb909193dcfde5f909860d1e60
psyclyx <me@psyclyx.xyz>
2025-07-02 19:51:04 -0700

    system: log component {pre,post}-{start,stop}

--- src/beepboop/system.clj
+++ src/beepboop/system.clj
@@ -4,13 +4,28 @@
     [beepboop.connection :as connection]
     [beepboop.server :as server]
     [clojure.java.io :as io]
+    [clojure.tools.logging :as log]
     [donut.system :as donut]
     [donut.system.validation :refer [validation-plugin]]
     [psyclyx.pastry :as pastry]))
 
 
+(defn -log-fn
+  [prefix]
+  (fn [{::donut/keys [component-id]}]
+    (log/info prefix component-id)))
+
+
+(def LogLifetimes
+  {::donut/pre-start {::log (-log-fn "Starting")}
+   ::donut/post-start {::log (-log-fn "Started")}
+   ::donut/pre-stop {::log (-log-fn "Stopping")}
+   ::donut/post-stop {::log (-log-fn "Stopped")}})
+
+
 (def system
   {::donut/plugins [pastry/pastry-plugin validation-plugin]
+   ::donut/base LogLifetimes
    ::donut/defs {:env {} ; to be replaced by config
                  :app {:server {::pastry/type ::server/tcp
                                 ::donut/config {:connection-handler connection/handle-connect
