73f04c2303d7d3a374c67db6b161f476fa19deb4
psyclyx <me@psyclyx.xyz>
2025-07-02 17:30:35 -0700

    test: add server-test ns with single smoke test
    just here so we have something for kaocha to run

---
+++ test/beepboop/server_test.clj
@@ -0,0 +1,14 @@
+(ns beepboop.server-test
+  (:require
+    [beepboop.server :as server]
+    [clojure.test :refer [deftest is]]))
+
+
+;; Kinda useless, but gives the test runner something to run at first...
+(deftest socket-address
+  (let [socket-addr (server/socket-address {:host "123.45.67.89"
+                                             :port 1234})]
+    (is (= 1234 (.getPort socket-addr))
+        "socket address port should match bind port")
+    (is (= "123.45.67.89" (.getHostString socket-addr))
+        "socket address host string should match bind host")))
