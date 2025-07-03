14f70bd6f0f7cfaaae1588a5e610b280ce231fc6
Sophie Winter <git@phie.me>
2025-07-03 02:08:33 -0700

    Move telnet stuff to its own file

--- src/beepboop/connection.clj
+++ src/beepboop/connection.clj
@@ -1,6 +1,7 @@
 (ns beepboop.connection
   (:require
     [beepboop.server :as server]
+    [beepboop.telnet :as telnet]
     [clojure.string :as str]
     [clojure.tools.logging :as log]))
 
@@ -10,91 +11,18 @@
 (declare draw-all)
 
 
-(def telnet-iac               (unchecked-byte 0xFF))
-(def telnet-will              (unchecked-byte 0xFB))
-(def telnet-wont              (unchecked-byte 0xFC))
-(def telnet-do                (unchecked-byte 0xFD))
-(def telnet-dont              (unchecked-byte 0xFE))
-(def telnet-echo              (unchecked-byte 0x01))
-(def telnet-suppress-go-ahead (unchecked-byte 0x03))
-(def telnet-linemode          (unchecked-byte 0x22))
-(def telnet-sb                (unchecked-byte 0xFA))
-(def telnet-se                (unchecked-byte 0xF0))
-(def telnet-naws              (unchecked-byte 0x1F))
-
-
 (def ansi-clear         "\033[2J")
 (def ansi-reset-cursor  "\033[H")
-
-
-(defn get-16-bit-int-from-bytes
-  [bytes index]
-  (bit-or (bit-shift-left (bit-and (aget bytes index) 0xFF) 8)
-          (bit-and (aget bytes (+ index 1)) 0xFF)))
-
-
-(defn parse-telnet-sb
-  [type bytes]
-  (case type
-    :sb-naws {:type :screen-size
-              :width (get-16-bit-int-from-bytes bytes 0)
-              :height (get-16-bit-int-from-bytes bytes 2)}
-    (do (log/info "Telnet SB" type)
-        nil)))
-
-
-;; See https://github.com/seanmiddleditch/libtelnet/blob/5f5ecee776b9bdaa4e981e5f807079a9c79d633e/libtelnet.c#L973
-(defn make-telnet-filter
-  []
-  (let [state (atom :data)
-        sb-type (atom nil)
-        sb-buffer (atom nil)]
-    (fn [byte meta-cb data-cb]
-      (case @state
-        :data (cond
-                (= byte telnet-iac) (reset! state :iac)
-                :else (data-cb byte))
-        :iac (cond
-               (= byte telnet-iac) (do (data-cb telnet-iac) ; escaped IAC
-                                       (reset! state :data))
-               (= byte telnet-sb) (reset! state :sb)
-               (= byte telnet-do) (reset! state :do)
-               (= byte telnet-dont) (reset! state :dont)
-               (= byte telnet-will) (reset! state :will)
-               (= byte telnet-wont) (reset! state :wont)
-               :else (do (log/info "Telnet command " byte)
-                         (reset! state :data)))
-        (:do :dont :will :wont) (do (log/info "Telnet command" @state byte)
-                                    (reset! state :data))
-        :sb (do (cond
-                  (= byte telnet-naws) (reset! sb-type :sb-naws)
-                  :else (reset! sb-type byte))
-                (reset! sb-buffer (byte-array []))
-                (reset! state :sb-data))
-        :sb-data (cond
-                   (= byte telnet-iac) (reset! state :sb-iac)
-                   :else (swap! sb-buffer #(byte-array (concat % [byte]))))
-        :sb-iac (cond
-                  (= byte telnet-se) (do (some-> (parse-telnet-sb @sb-type @sb-buffer) meta-cb)
-                                         (reset! sb-type nil)
-                                         (reset! sb-buffer nil)
-                                         (reset! state :data))
-                  :else (do (swap! sb-buffer #(byte-array (concat % [byte])))
-                            (reset! state :sb-data)))))))
 
 
 (defn handle-connect
   [channel]
   (log/info "New connection")
-  (server/send-message channel [telnet-iac telnet-will telnet-echo
-                                telnet-iac telnet-do telnet-suppress-go-ahead
-                                telnet-iac telnet-will telnet-suppress-go-ahead
-                                telnet-iac telnet-dont telnet-linemode
-                                telnet-iac telnet-do telnet-naws])
+  (telnet/initialize channel)
   {:channel channel
    :handle-packet handle-packet
    :handle-disconnect handle-disconnect
-   :filter-telnet (make-telnet-filter)
+   :filter-telnet (telnet/make-parser)
    :screen-width (atom 80)
    :screen-height (atom 80)})
 
---
+++ src/beepboop/telnet.clj
@@ -0,0 +1,82 @@
+(ns beepboop.telnet
+  (:require
+    [beepboop.server :as server]
+    [clojure.tools.logging :as log]))
+
+
+(def telnet-iac               (unchecked-byte 0xFF))
+(def telnet-will              (unchecked-byte 0xFB))
+(def telnet-wont              (unchecked-byte 0xFC))
+(def telnet-do                (unchecked-byte 0xFD))
+(def telnet-dont              (unchecked-byte 0xFE))
+(def telnet-echo              (unchecked-byte 0x01))
+(def telnet-suppress-go-ahead (unchecked-byte 0x03))
+(def telnet-linemode          (unchecked-byte 0x22))
+(def telnet-sb                (unchecked-byte 0xFA))
+(def telnet-se                (unchecked-byte 0xF0))
+(def telnet-naws              (unchecked-byte 0x1F))
+
+
+(defn get-16-bit-int-from-bytes
+  [bytes index]
+  (bit-or (bit-shift-left (bit-and (aget bytes index) 0xFF) 8)
+          (bit-and (aget bytes (+ index 1)) 0xFF)))
+
+
+(defn parse-sb
+  [type bytes]
+  (case type
+    :sb-naws {:type :screen-size
+              :width (get-16-bit-int-from-bytes bytes 0)
+              :height (get-16-bit-int-from-bytes bytes 2)}
+    (do (log/info "Telnet SB" type)
+        nil)))
+
+
+;; See https://github.com/seanmiddleditch/libtelnet/blob/5f5ecee776b9bdaa4e981e5f807079a9c79d633e/libtelnet.c#L973
+(defn make-parser
+  []
+  (let [state (atom :data)
+        sb-type (atom nil)
+        sb-buffer (atom nil)]
+    (fn [byte meta-cb data-cb]
+      (case @state
+        :data (cond
+                (= byte telnet-iac) (reset! state :iac)
+                :else (data-cb byte))
+        :iac (cond
+               (= byte telnet-iac) (do (data-cb telnet-iac) ; escaped IAC
+                                       (reset! state :data))
+               (= byte telnet-sb) (reset! state :sb)
+               (= byte telnet-do) (reset! state :do)
+               (= byte telnet-dont) (reset! state :dont)
+               (= byte telnet-will) (reset! state :will)
+               (= byte telnet-wont) (reset! state :wont)
+               :else (do (log/info "Telnet command " byte)
+                         (reset! state :data)))
+        (:do :dont :will :wont) (do (log/info "Telnet command" @state byte)
+                                    (reset! state :data))
+        :sb (do (cond
+                  (= byte telnet-naws) (reset! sb-type :sb-naws)
+                  :else (reset! sb-type byte))
+                (reset! sb-buffer (byte-array []))
+                (reset! state :sb-data))
+        :sb-data (cond
+                   (= byte telnet-iac) (reset! state :sb-iac)
+                   :else (swap! sb-buffer #(byte-array (concat % [byte]))))
+        :sb-iac (cond
+                  (= byte telnet-se) (do (some-> (parse-sb @sb-type @sb-buffer) meta-cb)
+                                         (reset! sb-type nil)
+                                         (reset! sb-buffer nil)
+                                         (reset! state :data))
+                  :else (do (swap! sb-buffer #(byte-array (concat % [byte])))
+                            (reset! state :sb-data)))))))
+
+
+(defn initialize
+  [channel]
+  (server/send-message channel [telnet-iac telnet-will telnet-echo
+                                telnet-iac telnet-do telnet-suppress-go-ahead
+                                telnet-iac telnet-will telnet-suppress-go-ahead
+                                telnet-iac telnet-dont telnet-linemode
+                                telnet-iac telnet-do telnet-naws]))
